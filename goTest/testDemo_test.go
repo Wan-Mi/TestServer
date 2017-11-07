a2e16152704dc8582d2953ed902d6667dd9eba4e
WanMi <mi.wan@ele.me>
2017-11-07 14:24:40 +0800

    benchmarkTest

--- goTest/testDemo_test.go
+++ goTest/testDemo_test.go
@@ -9,7 +9,21 @@ func helloWorld() string {
 	return "helloWorld"
 }
 
-func Test_HelloWord(t *testing.T) {
-	fmt.Println("test begin:\n")
-	t.Errorf("test result %s", helloWorld())
+// normal test
+func TestUnit(t *testing.T) {
+	fmt.Println("test begin:")
+	if helloWorld() != "helloWorld" {
+		t.Errorf("test result: %v", helloWorld())
+	} else {
+		fmt.Println("test PASS")
+	}
+}
+
+// benchmark test
+func BenchmarkUnit(b *testing.B) {
+	b.N = 100
+
+	for i := 0; i < b.N; i++ {
+		helloWorld()
+	}
 }
