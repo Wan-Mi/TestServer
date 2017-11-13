28f378079c75c4090b30dc8a2d86c5cf68ea9901
WanMi <mi.wan@ele.me>
2017-11-13 18:31:03 +0800

    syncGoroutine

--- cmds/main.go
+++ cmds/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/wanmii/TestServer/protoBuff"
+	"github.com/wanmii/TestServer/syncGoroutine"
 )
 
 //使用
@@ -9,6 +9,8 @@ func main() {
 
 	//jwt.Test()
 	//iInterface.Test()
-	protoBuff.Test()
+	//protoBuff.Test()
 	//router.MakeRouter()
+
+	syncGoroutine.TestWaitGroup()
 }
---
+++ syncGoroutine/byWaitGroup.go
@@ -0,0 +1,30 @@
+package syncGoroutine
+
+import (
+	"fmt"
+	"sync"
+)
+
+func TestWaitGroup() {
+	testSlice := []int64{
+		1,
+		2,
+		3,
+		4,
+		5,
+	}
+	var res int64
+
+	var wg sync.WaitGroup
+	wg.Add(len(testSlice))
+	for _, val := range testSlice {
+		go func(t int64) {
+			defer wg.Done()
+			res += t
+
+		}(val)
+	}
+	wg.Wait()
+
+	fmt.Println("results is :", res)
+}
