6be617adc49835edbdcaf766d9e374f2e75b7348
WanMi <mi.wan@ele.me>
2017-11-21 12:34:39 +0800

    getTimeWindowTest

--- cmds/main.go
+++ cmds/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"github.com/wanmii/TestServer/syncGoroutine"
+	"fmt"
+	"sort"
+	"time"
 )
 
 //使用
@@ -12,5 +14,37 @@ func main() {
 	//protoBuff.Test()
 	//router.MakeRouter()
 
-	syncGoroutine.ChanTests()
+	//	syncGoroutine.ChanTests()
+
+	getTimeWindowTest()
+}
+
+func getTimeWindowTest() {
+	valMap := map[int64]string{
+		1511236559: "aaaa",
+		1511238459: "bbbb",
+	}
+	var timeStamps []int
+	for key, _ := range valMap {
+		timeStamps = append(timeStamps, int(key))
+	}
+	sort.Ints(timeStamps)
+
+	getWindowDeviceID := func(tSs []int, tS int) string {
+		if len(tSs) == 0 {
+			return ""
+		}
+
+		tSs = append(tSs, int(time.Now().Unix()+1))
+		for idx, t := range tSs {
+
+			if idx > 0 && t > tS {
+				key := int64(tSs[idx-1])
+				return valMap[key]
+			}
+		}
+		return ""
+	}
+
+	fmt.Println(getWindowDeviceID(timeStamps, int(time.Now().Unix())))
 }
