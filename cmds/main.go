e635112a161fb6963b46bd4a07759506907ef198
WanMi <mi.wan@ele.me>
2017-12-05 15:04:54 +0800

    gotests

--- cmds/main.go
+++ cmds/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"time"
 )
 
 //使用
@@ -16,35 +14,10 @@ func main() {
 
 	//	syncGoroutine.ChanTests()
 
-	getTimeWindowTest()
+	getTest("hello")
 }
 
-func getTimeWindowTest() {
-	valMap := map[int64]string{
-		1511236559: "aaaa",
-		1511238459: "bbbb",
-	}
-	var timeStamps []int
-	for key, _ := range valMap {
-		timeStamps = append(timeStamps, int(key))
-	}
-	sort.Ints(timeStamps)
+func getTest(str string) {
 
-	getWindowDeviceID := func(tSs []int, tS int) string {
-		if len(tSs) == 0 {
-			return ""
-		}
-
-		tSs = append(tSs, int(time.Now().Unix()+1))
-		for idx, t := range tSs {
-
-			if idx > 0 && t > tS {
-				key := int64(tSs[idx-1])
-				return valMap[key]
-			}
-		}
-		return ""
-	}
-
-	fmt.Println(getWindowDeviceID(timeStamps, int(time.Now().Unix())))
+	fmt.Println(str)
 }
